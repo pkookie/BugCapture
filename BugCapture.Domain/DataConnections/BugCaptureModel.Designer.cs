46adff31ebd8a11b73309d7154628327b016d1b6
Philip Cooke <philip.cooke@trafford.gov.uk>
2014-06-18 08:41:35 +0100

    created entity model

---
+++ BugCapture.Domain/DataConnections/BugCaptureModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\Projects\Personal Sandbox\BugCapture\BugCapture.Domain\DataConnections\BugCaptureModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
